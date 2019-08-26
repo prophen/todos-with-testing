06fc7f3e9d9164f7aff54c69ef312c57d601e128
Nikema Prophet <nikema.prophet@popschools.com>
2019-08-25 21:27:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ dbPool.on('error', (err, client) => {
 })
 
 // Retrieve all todos as database records from postgresql and provide them to the passed in callback function
+const getTodos = (cb) => dbPool.query('SELECT id, entry FROM todo_items', cb)
+
+// Save a todo to the postgresql database, and provide the resulting database record to a callback function
+const saveTodo = (todo, cb) => dbPool.query('INSERT INTO todo_items (entry) VALUES($1)', [todo], cb)
 
 // load express
 const express = require('express')
@@ -20,14 +24,16 @@ app.use(bodyParser.json())
 // the default endpoint will just return a JSON representation of the TODO items that we know about
 
 app.get('/', (req, res) => {
-  dbPool.query('SELECT id, entry FROM todo_items', (err, queryResult) => {
+  getTodos((err, todoResult) => {
+    const hasError = !!err
     const result = {
-      error: !!err,
-      todo: queryResult.rows,
+      error: hasError,
+      // if we got a DB error return no data to avoid inconsistent results
+      todo: hasError ? [] : todoResult.rows,
     }
 
-    const respCode = result.error ? 503 : 200
-    res.send(respCode, JSON.stringify(result))
+    const respCode = hasError ? 503 : 200
+    res.status(respCode).send(JSON.stringify(result))
   })
 })
 
@@ -43,3 +49,23 @@ app.get('/items', (req, res) => {
     res.send(`<b>TODO list:</b><br/><ul>${items}</ul>`)
   })
 })
+
+// register a route to handle POST requests to '/' It creates a TODO in the database and redirects users
+
+app.post('/', (req, res) => {
+  if(!req.body || !req.body.todo) {
+    res.status(400).send('Expected TODO item')
+    return
+  }
+
+  saveTodo(req.body.todo, (err, dbRes) => {
+    if (err) {
+      res.status(503).send(`Unable to save new TODO item: ${req.body.todo}`)
+      return
+    }
+    res.redirect('/')
+  })
+})
+
+const port = 3000;
+app.listen(port, () => console.log(`Todo app listening on port ${port}...`))
