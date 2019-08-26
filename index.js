7554824f782330626318ea167a003f8fe76d04a8
Nikema Prophet <nikema.prophet@popschools.com>
2019-08-25 22:14:53 -0700

    Update port variable

--- index.js
+++ index.js
@@ -72,5 +72,5 @@ const constructRoutes = (app, getTodos) => {
 
 constructRoutes(app, getTodos);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Todo app listening on port ${port}...`));
